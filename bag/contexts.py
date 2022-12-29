c80578044f04282b113e5c832ead00e2c358367a
EirEoin <eoinc2010@live.ie>
2022-12-29 19:48:43 +0000

    added quantity selector

--- bag/contexts.py
+++ bag/contexts.py
@@ -3,7 +3,6 @@ from django.conf import settings
 from django.shortcuts import get_object_or_404
 from products.models import Product
 
-
 def bag_contents(request):
 
     bag_items = []
@@ -19,6 +18,7 @@ def bag_contents(request):
             bag_items.append({
                 'item_id': item_id,
                 'quantity': item_data,
+                'product': product,
             })
         else:
             product = get_object_or_404(Product, pk=item_id)
@@ -28,11 +28,19 @@ def bag_contents(request):
                 bag_items.append({
                     'item_id': item_id,
                     'quantity': quantity,
+                    'product': product,
                     'size': size,
                 })
 
+    if total < settings.FREE_DELIVERY_THRESHOLD:
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+    else:
+        delivery = 0
+        free_delivery_delta = 0
+    
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
