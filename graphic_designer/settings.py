fed6fb4af7ccff013105979c5a9442976a55e2be
EirEoin <eoinc2010@live.ie>
2023-01-26 19:21:02 +0000

    added custom 404 page

--- graphic_designer/settings.py
+++ graphic_designer/settings.py
@@ -52,7 +52,6 @@ INSTALLED_APPS = [
     'profiles',
     'gallery',
 
-
     # Other
     'crispy_forms',
 ]
--- graphic_designer/urls.py
+++ graphic_designer/urls.py
@@ -16,6 +16,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from .views import handler404
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -27,3 +28,5 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('work/', include('gallery.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+handler404 = 'graphic_designer.views.handler404'
