821f4497ff9990d14162262e06516adf5b9c8d87
EirEoin <eoinc2010@live.ie>
2023-01-30 19:22:17 +0000

    made gallery a custom model

--- gallery/views.py
+++ gallery/views.py
@@ -4,6 +4,5 @@ from .models import PreviousWork
 # Create your views here.
 
 def work(request):
-    images = ["Rinse_JohnDaly.jpg", "Rinse_Shampain.jpg", "Guiness_Skeleton.jpg", "Rinse_Crabs.jpg", "Rinse_Ducks.jpg", "Skeleton_Feet.jpg"]
-    return render(request, 'work.html', {'images': images})
-
+    gallery = PreviousWork.objects.all()
+    return render(request, 'work.html', {'gallery': gallery})
