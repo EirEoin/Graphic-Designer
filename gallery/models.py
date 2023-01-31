43108afeeff7364cc295c7aa453ff06946ea5d85
EirEoin <eoinc2010@live.ie>
2023-01-31 00:13:44 +0000

    added to models and views trying to fix error

--- gallery/models.py
+++ gallery/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 
+
 # Create your models here.
 class PreviousWork(models.Model):
     title = models.CharField(max_length=100)
     description = models.TextField()
     image = models.ImageField(upload_to='previous_work_images/')
     date = models.DateField()
+
 
 def __str__(self):
     return self.title
--- gallery/views.py
+++ gallery/views.py
@@ -3,6 +3,7 @@ from .models import PreviousWork
 
 # Create your views here.
 
+
 def work(request):
     gallery = PreviousWork.objects.all()
     return render(request, 'work.html', {'gallery': gallery})
