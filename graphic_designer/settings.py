a470dee80ce6f7d594085c842a84424d13c590d9
EirEoin <eoinc2010@live.ie>
2023-02-27 20:11:50 +0000

    put email host user and password in env.py file?

--- graphic_designer/settings.py
+++ graphic_designer/settings.py
@@ -202,6 +202,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = 'sketchyactive@gmail.com'
-    EMAIL_HOST_PASSWORD = 'mdqrfywlkfwfxkml'
+    EMAIL_HOST_USER = os.environ.get('sketchyactive@gmail.com')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
     DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
