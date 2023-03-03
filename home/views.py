5411530a909d34c0cfe04951c7eeef230133ea71
EirEoin <eoinc2010@live.ie>
2023-03-03 11:53:24 +0000

    updated views and added product then delted it. Media causing 404 issue

--- home/views.py
+++ home/views.py
@@ -6,7 +6,3 @@ def index(request):
     """ A view to return the index page """
 
     return render(request, 'home/index.html')
-
-def work(request):
-    images = ["Rinse_JohnDaly.jpg", "Rinse_Shampain.jpg", "Guiness_Skeleton.jpg", "Rinse_Crabs.jpg", "Rinse_Ducks.jpg", "Skeleton_Feet.jpg"]
-    return render(request, 'work.html', {'images': images})
