b154cb8fd9582b0c9ea704f94d1a70df353112c7
EirEoin <eoinc2010@live.ie>
2023-03-11 15:02:39 +0000

    changing setting and added custom_storages file

--- graphic_designer/settings.py
+++ graphic_designer/settings.py
@@ -58,7 +58,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
-    'storage',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -184,12 +184,12 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    #Bucket Config
+    # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'sketchydesigns'
     AWS_S3_REGION_NAME = 'EU (Ireland) eu-west-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY')
-    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazon.com'
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com'
 
     # Static and media files
     STATICFILES_STORAGE = 'custom_storages.StaticStorage'
