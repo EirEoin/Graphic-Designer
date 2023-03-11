48dae63091a6c374a155fa63234ca9a9e7c20596
EirEoin <eoinc2010@live.ie>
2023-03-11 14:44:43 +0000

    added aws and other storage for deployment

--- graphic_designer/settings.py
+++ graphic_designer/settings.py
@@ -31,7 +31,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = ['sketchydesigns.herokuapp.com', 'localhost']
 
-CSRF_TRUSTED_ORIGINS = ['https://8000-eireoin-graphicdesigner-x5te3k7chi0.ws-eu89b.gitpod.io', 'https://*.127.0.0.1']
+CSRF_TRUSTED_ORIGINS = ['https://8000-eireoin-graphicdesigner-x5te3k7chi0.ws-eu90.gitpod.io', 'https://*.127.0.0.1']
 
 
 # Application definition
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
 
     # Other
     'crispy_forms',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -181,6 +182,24 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+if 'USE_AWS' in os.environ:
+    #Bucket Config
+    AWS_STORAGE_BUCKET_NAME = 'sketchydesigns'
+    AWS_S3_REGION_NAME = 'EU (Ireland) eu-west-1'
+    AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+    AWS_SECRET_ACCESS_KEY_ID = os.environ.get('AWS_SECRET_ACCESS_KEY')
+    AWS_S3_CUSTOM_DOMAIN = f'{AWS_STORAGE_BUCKET_NAME}.s3.amazon.com'
+
+    # Static and media files
+    STATICFILES_STORAGE = 'custom_storages.StaticStorage'
+    STATICFILES_LOCATION = 'static'
+    DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
+    MEDIAFILES_LOCATION = 'media'
+
+    # Override static and media URLs in production
+    STATIC_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{STATICFILES_LOCATION}/'
+    MEDIA_URL = f'https://{AWS_S3_CUSTOM_DOMAIN}/{MEDIAFILES_LOCATION}/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
