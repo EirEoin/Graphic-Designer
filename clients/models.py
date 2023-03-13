5dacb898d0c40a9c1c35bfa28b1ef90b0f46396a
EirEoin <eoinc2010@live.ie>
2023-03-13 18:12:14 +0000

    addded to models

--- clients/models.py
+++ clients/models.py
@@ -9,9 +9,9 @@ class Clients(models.Model):
     description = models.TextField()
     location = models.CharField(max_length=100)
 
-    def __str__(self):
-        return self.name
-
 
 class Meta:
     verbose_name_plural = 'clients'
+
+    def __str__(self):
+        return self.name
