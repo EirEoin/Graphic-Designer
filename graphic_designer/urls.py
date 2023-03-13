7f529e573f170713e6d7080bf290bd24973dd3ef
EirEoin <eoinc2010@live.ie>
2023-03-13 17:26:14 +0000

    added clients urls

--- graphic_designer/urls.py
+++ graphic_designer/urls.py
@@ -12,11 +12,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from .views import handler404, clients, contact
+from .views import handler404
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from clients.views import clients
 
 urlpatterns = [
     path('admin/', admin.site.urls),
