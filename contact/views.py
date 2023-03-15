ceecdb5fa29f732e0f832b31e121070b4318f57d
EirEoin <eoinc2010@live.ie>
2023-03-15 11:29:58 +0000

    added to contact views

--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import ContactForm
+from .models import Contact
 
 
 def contact(request):
     if request.method == 'POST':
         form = ContactForm(request.POST)
         if form.is_valid():
-            print("IT WORKS")
-            pass
+            # Save the form data to the database
+            contact = form.save()
+            # Display a success message to the user
+            messages.success(request, 'Thank you for reaching out!')
+            # Redirect the user back to the contact page with an empty form
+            return redirect('contact')
     else:
         form = ContactForm()
 
